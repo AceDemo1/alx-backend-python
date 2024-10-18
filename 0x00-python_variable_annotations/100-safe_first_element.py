ab90e034780f10b76b233c822eda06d5a418097f
AceDemo1 <lawal641@gmail.com>
2024-10-18 22:57:28 +0000

    function annotation

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from typing import Sequence, Any, Union
 """annotate func"""
+from typing import Sequence, Any, Union
+
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """define func"""
     if lst:
