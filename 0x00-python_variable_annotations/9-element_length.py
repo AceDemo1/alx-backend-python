da90cc5126eb143dfcb8ca25f74be5c443da5290
AceDemo1 <lawal641@gmail.com>
2024-10-18 22:44:14 +0000

    function annotation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,4 @@ from typing import Iterable, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[List[Sequence], int]]:
     """define func"""
-        return [(i, len(i)) for i in lst]
+    return [(i, len(i)) for i in lst]
