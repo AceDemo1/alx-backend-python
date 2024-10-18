ec4f820ed10121fb90ed664dc38d047c9cc9905f
AceDemo1 <lawal641@gmail.com>
2024-10-18 22:11:47 +0000

    return a func

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""returns a func"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """define func"""
+    ret = lambda n: n *multiplier 
+    return ret
