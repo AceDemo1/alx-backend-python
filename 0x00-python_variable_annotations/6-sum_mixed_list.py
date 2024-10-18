c02d4f8897b8937e6d91126e37cb40f3dc012b41
AceDemo1 <lawal641@gmail.com>
2024-10-18 21:02:13 +0000

    sum of floats

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""takes list and return their sum as a float"""
+from typing import List
+
+
+def sum_mixed_list(mxd_list: List[float, int]) -> float:
+    """define func"""
+    j = 0
+    for i in input_list:
+        j += i
+    return j
