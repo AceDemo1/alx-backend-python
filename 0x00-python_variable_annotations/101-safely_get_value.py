9e3add5b05263fad8ffd428d1003fb46fdb23e21
AceDemo1 <lawal641@gmail.com>
2024-10-19 15:07:54 +0000

    function annotation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""annotate func"""
+from typing import Union, Mapping, Any, TypeVar
+
+
+ty = TypeVar('ty')
+def safely_get_value(dct: Mapping[Any, ty], key: Any, default: Union[ty, None] = None) -> Union[ty, None]:
+    """define func"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
