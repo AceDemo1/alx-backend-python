79a23e726467a9b1be0f839fd29163fd2bc40a5c
AceDemo1 <lawal641@gmail.com>
2024-10-20 23:49:43 +0000

    random delay

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+__import__('0-basic_async_syntax').wait_random
+import asyncio, random
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """define func"""
+    i = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    j = [] 
+    for k in asyncio.as_completed(i):
+        l = await k
+        j.append(l)
+    return j
