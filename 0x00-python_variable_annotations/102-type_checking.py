16206ad49017ced04a3e35b2c0afc2971564f215
AceDemo1 <lawal641@gmail.com>
2024-10-20 10:59:07 +0000

    function annotation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,7 +2,9 @@
 """annotate func"""
 from typing import List, Tuple
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """define func"""
     zoomed_in: List[int] = [
             item for item in lst
             for i in range(factor)
