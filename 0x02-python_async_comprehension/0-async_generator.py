b8f5590dd50531a8a528ac88e0d96c8845baad7f
AceDemo1 <lawal641@gmail.com>
2024-10-21 21:18:00 +0000

    yield values

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""yield values"""
+'''Module which handles a coroutine generator function which returns an int'''
+
 import asyncio
+import typing
 import random
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """define func"""
-    for _ in range(10):
+async def async_generator() -> typing.AsyncGenerator[float, None]:
+    '''Coroutine function that returns a randomize value'''
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
