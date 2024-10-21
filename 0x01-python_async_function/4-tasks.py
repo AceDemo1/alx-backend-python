22ea17d7d8620de25d8c3131fe4e99b98f092125
AceDemo1 <lawal641@gmail.com>
2024-10-21 00:50:22 +0000

    random delay

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_random(n: int, max_delay: int) -> List[float]:
+async def task_wait_random(max_delay: int) -> List[float]:
     """define func"""
     delays = [task_wait_random(max_delay) for j in range(n)]
     tasks = []
