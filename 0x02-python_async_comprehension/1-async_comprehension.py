1b8d15e9a017a71faae12c38003bee74664c2242
AceDemo1 <lawal641@gmail.com>
2024-10-21 22:27:32 +0000

    yield values

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprhension() -> List[float]:
+async def async_comprehension() -> List[float]:
     """define func"""
     collections = [i async for i in async_generator()]
     return collections
