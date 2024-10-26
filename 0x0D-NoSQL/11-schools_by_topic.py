c6639a50f74d06f29599a4a41953ad6173776947
AceDemo1 <lawal641@gmail.com>
2024-10-26 04:34:13 +0000

    nosql

--- 0x0D-NoSQL/11-schools_by_topic.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-""" pymongo module """
-
-
-def schools_by_topic(mongo_collection, topic):
-    """ Return the list of school having a specific topic """
-    documents = mongo_collection.find({"topics": topic})
-    return documents
