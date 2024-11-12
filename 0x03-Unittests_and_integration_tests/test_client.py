dd470be5290833731aee0582c01041851fba7589
AceDemo1 <lawal641@gmail.com>
2024-11-12 15:41:34 +0000

    client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,10 +31,10 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mock_get):
         """public repos"""
-        mock_get.return_value = f"https://api.github.com/orgs/test"
+        mock_get.return_value = [{'name': 'law'}]
         with patch.object(GithubOrgClient, _public_repos_url, new_callable=PropertyMock) as mock_obj:
-            mock_obj.return_value = {}
+            mock_obj.return_value = "https://api.github.com/orgs/test"
             obj = GithubOrgClient('test')
-            self.asserEqual(obj._public_repos_url, {})
-            mock_get.assert_called_once()
+            self.assertEqual(obj.public_repos, ['law'])
+            mock_get.assert_called_once_with( mock_obj.return_value)
             mock_obj.assert_called_once()
