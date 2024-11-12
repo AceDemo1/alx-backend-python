56a1ff1fbca7283a55f3221a36b55a78e3fb3e6e
AceDemo1 <lawal641@gmail.com>
2024-11-12 15:42:37 +0000

    client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,12 +12,12 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ('google',),
         ('abc',)
-        ])
+    ])
     @patch('client.get_json')
     def test_org(self, name, mock_obj):
         """test_org"""
         mock_obj.return_value = {}
-        res =  GithubOrgClient(name).org
+        res = GithubOrgClient(name).org
         mock_obj.assert_called_once_with(f"https://api.github.com/orgs/{name}")
         self.assertEqual(res, mock_obj.return_value)
 
@@ -36,5 +36,5 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_obj.return_value = "https://api.github.com/orgs/test"
             obj = GithubOrgClient('test')
             self.assertEqual(obj.public_repos, ['law'])
-            mock_get.assert_called_once_with( mock_obj.return_value)
+            mock_get.assert_called_once_with(mock_obj.return_value)
             mock_obj.assert_called_once()
