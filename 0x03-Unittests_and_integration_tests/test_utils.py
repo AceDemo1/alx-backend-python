4a24ceb02112f7b7cba937b38124106f6103b5b3
AceDemo1 <lawal641@gmail.com>
2024-11-12 01:48:55 +0000

    mck

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -33,7 +33,7 @@ class TestGetJson(unittest.TestCase):
     def test_get_json(self, url, response, mock_get):
         """mock"""
         mock_get.return_value.json.return_value = response
-        self.assertEqual(get_json(url), response)
+        self.assertEqual(mock_get(url), response)
         mock_get.assert_called_once_with(url)
 
 class TestMemoize(unittest.TestCase):
